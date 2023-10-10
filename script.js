50b74d2cae071b2ad586ee8078420b72f74f66e5
skoanton <antonhakanssonn@gmail.com>
2023-10-10 13:53:23 +0200

    commit1

--- script.js
+++ script.js
@@ -11,29 +11,29 @@ function addBodyParts() {
         let addPart;
         switch (tries) {
             case 1:
-                     addPart = document.querySelector("svg > #ground");
-                     addPart.style.visibility ="visible";
-                
+                addPart = document.querySelector("svg > #ground");
+                addPart.style.visibility = "visible";
+
                 break;
             case 2:
                 addPart = document.querySelector("svg > #scaffold");
-                     addPart.style.visibility ="visible";
+                addPart.style.visibility = "visible";
                 break;
             case 3:
                 addPart = document.querySelector("svg > #head");
-                addPart.style.visibility ="visible";
+                addPart.style.visibility = "visible";
                 break;
             case 4:
                 addPart = document.querySelector("svg > #body");
-                addPart.style.visibility ="visible";
+                addPart.style.visibility = "visible";
                 break;
             case 5:
                 addPart = document.querySelector("svg > #arms");
-                     addPart.style.visibility ="visible";
+                addPart.style.visibility = "visible";
                 break;
             case 6:
                 addPart = document.querySelector("svg > #legs");
-                addPart.style.visibility ="visible";
+                addPart.style.visibility = "visible";
                 break;
             default:
                 console.log("fail");
