b09ddf1c4241067a32ec214b1dbf43ff6f6e5c5a
Ghayth Sabeaallil <ghaessesa58@gmail.com>
2023-10-10 13:54:28 +0200

    v 1

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-"use strict";
+function print(letter) {
+  console.log(letter);
+}
+const words = ["hangman", "javascript", "programming", "developer", "computer"];
 
-let btn = xndhashiosahjio;
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+console.log(selectedWord);
 
-let y = s;
+charWord = selectedWord.split("");
 
-for (let i = 0; i < 0; i++) {}
-let x = 0;
+for (let i = 0; i < charWord.length; i++) {
+  const box = document.createElement("div");
+  box.className = "letter-box";
+  box.id = i + 1;
+  document.querySelector(".word-box").append(box);
+}
+let box = document.querySelector(".word-box");
+function print(letter) {
+  for (let i = 0; i < charWord.length; i++) {
+    if (charWord[i] === letter) {
+    }
+  }
+}
