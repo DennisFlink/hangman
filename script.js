3dd9941ade35f57918259d69d919578da0a0627a
skoanton <antonhakanssonn@gmail.com>
2023-10-10 14:47:06 +0200

    hej2sajnssdsdsd

--- script.js
+++ script.js
@@ -2,9 +2,6 @@
 const maxTries = 6;
 let tries = 1;
 
-let testButton = document.querySelector("#test")
-testButton.addEventListener("click", addBodyParts);
-
 function addBodyParts() {
     if (tries <= maxTries) {
 
@@ -12,28 +9,29 @@ function addBodyParts() {
         switch (tries) {
             case 1:
                 addPart = document.querySelector("svg > #ground");
-                addPart.style.visibility = "visible";
-
+                addPart.style.display ="block";
+                console.log(tries);
                 break;
             case 2:
                 addPart = document.querySelector("svg > #scaffold");
-                addPart.style.visibility = "visible";
+                addPart.style.display ="block";
+                console.log(tries);
                 break;
             case 3:
                 addPart = document.querySelector("svg > #head");
-                addPart.style.visibility = "visible";
+                addPart.style.display ="block";
                 break;
             case 4:
                 addPart = document.querySelector("svg > #body");
-                addPart.style.visibility = "visible";
+                addPart.style.display ="block";
                 break;
             case 5:
                 addPart = document.querySelector("svg > #arms");
-                addPart.style.visibility = "visible";
+                addPart.style.display ="block";
                 break;
             case 6:
                 addPart = document.querySelector("svg > #legs");
-                addPart.style.visibility = "visible";
+                addPart.style.display ="block";
                 break;
             default:
                 console.log("fail");
@@ -60,11 +58,17 @@ for (let i = 0; i < charWord.length; i++) {
   box.id = i + 1;
   document.querySelector(".word-box").append(box);
 }
+
+
 function print(letter) {
-  for (let i = 0; i < charWord.length; i++) {
-    if (charWord[i] === letter) {
-      document.querySelector(`.word-box > div:nth-child(${i + 1})`).innerHTML =
-        letter;
+    let guess = 0;
+    charWord.forEach((char, i) => {
+      if (char === letter.toLowerCase()) {
+        document.getElementById(i + 1).textContent = char;
+        guess = 1;
+      }
+    });
+    if (guess == 0) {
+      addBodyParts();
     }
-  }
 }
