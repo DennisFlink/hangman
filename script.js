f8eb0821bf57485bb8de9ce421c2f15895550d60
Ghayth Sabeaallil <ghaessesa58@gmail.com>
2023-10-10 14:05:24 +0200

    keyboard + boxes + letter position

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ function print(letter) {
 const words = ["hangman", "javascript", "programming", "developer", "computer"];
 
 let selectedWord = words[Math.floor(Math.random() * words.length)];
-console.log(selectedWord);
 
 charWord = selectedWord.split("");
 
@@ -14,10 +13,11 @@ for (let i = 0; i < charWord.length; i++) {
   box.id = i + 1;
   document.querySelector(".word-box").append(box);
 }
-let box = document.querySelector(".word-box");
 function print(letter) {
   for (let i = 0; i < charWord.length; i++) {
     if (charWord[i] === letter) {
+      document.querySelector(`.word-box > div:nth-child(${i + 1})`).innerHTML =
+        letter;
     }
   }
 }
