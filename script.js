7a53b0b325f6c765a944048b3923d86537981eb7
mrdavinski <skoanton2@gmail.com>
2023-10-11 12:07:33 +0200

    Added total trires, now the countdown of tries changes color

--- script.js
+++ script.js
@@ -2,36 +2,62 @@
 const maxTries = 6;
 let tries = 1;
 
+/* KEYBOARD JAVASCRIPT */
+// prettier-ignore
+const keyboardLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'Ä', 'Å', 'Ö'];
+const keyboardBox = document.querySelector(".keyboard-box");
+
+keyboardLetters.forEach((letters) => {
+  const button = document.createElement("button"); // create buttons
+  button.setAttribute("id", letters.toLowerCase()); // id to buttons
+  button.textContent = letters; // Adding textcontent to button elements
+  button.setAttribute("class", "letter"); // "letter" class for styling in css
+  button.addEventListener("click", () => {
+    print(letters);
+  });
+  keyboardBox.appendChild(button); // child of keyboardBox
+});
+
 function addBodyParts() {
     if (tries <= maxTries) {
 
         let addPart;
+        let triesText = document.querySelector("#tries");
         switch (tries) {
             case 1:
                 addPart = document.querySelector("svg > #ground");
                 addPart.style.display ="block";
-                console.log(tries);
+                triesText.innerHTML ="5";
                 break;
             case 2:
                 addPart = document.querySelector("svg > #scaffold");
                 addPart.style.display ="block";
-                console.log(tries);
+                triesText.innerHTML ="4";
+                triesText.style.color = "#D4E03C";
                 break;
             case 3:
                 addPart = document.querySelector("svg > #head");
                 addPart.style.display ="block";
+                triesText.innerHTML ="3";
+                triesText.style.color = "#D4E03C";
                 break;
             case 4:
                 addPart = document.querySelector("svg > #body");
                 addPart.style.display ="block";
+                triesText.innerHTML ="2";
+                triesText.style.color = "#EEC82D";
                 break;
             case 5:
                 addPart = document.querySelector("svg > #arms");
                 addPart.style.display ="block";
+                triesText.innerHTML ="1";
+                triesText.style.color = "#EEA82D";
                 break;
             case 6:
                 addPart = document.querySelector("svg > #legs");
                 addPart.style.display ="block";
+                triesText.innerHTML ="0";
+                triesText.style.color = "#F74216";
                 break;
             default:
                 console.log("fail");
