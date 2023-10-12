12d0cad184148df80de9bcdb5b81b729a081028f
DennisFlink <dennis.sabriev@outlook.com git config --global user.name>
2023-10-12 11:10:49 +0200

    complete

--- script.js
+++ script.js
@@ -5,19 +5,22 @@ const modal = document.querySelector('.modal-box');
 const modalHeader = document.querySelector('.modal-box-header');
 const newGameBtn = document.querySelector('.new-game');
 const message = document.querySelector('.message');
+const svgId = document.querySelectorAll('svg [id]');
+
 /* DECLARING GLOBAL VARIABLES */
 const maxGuesses = 6;
 let wrongGuessCounter = 0;
 let rightGuesses = 0;
 let selectedWord;
 let charWord = [];
+let keysPressed = [];
+let playing = true;
 /////////////////////////
 
 /* KEYBOARD JAVASCRIPT */
 // prettier-ignore
 const keyboardLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'Ä', 'Å', 'Ö'];
 const keyboardBox = document.querySelector('.keyboard-box');
-
 keyboardLetters.forEach(letters => {
   const button = document.createElement('button'); // create buttons
   button.setAttribute('id', letters.toLowerCase()); // id to buttons
@@ -30,14 +33,18 @@ keyboardLetters.forEach(letters => {
 });
 const letterButtons = document.querySelectorAll('.letter');
 
+// Event Delegation for keydown //
 document.addEventListener('keydown', event => {
   const key = event.key.toUpperCase();
-  if (keyboardLetters.includes(key)) {
+  if (keyboardLetters.includes(key) && !keysPressed.includes(key)) {
+    keysPressed.push(key);
     print(key);
   }
 });
+
 // NEW GAME FUNCTION //
 function newGame() {
+  playing = true;
   wrongGuessCounter = 0;
   rightGuesses = 0;
   selectedWord = generateWord();
@@ -48,6 +55,10 @@ function newGame() {
   while (wordBox.firstChild) {
     wordBox.removeChild(wordBox.firstChild);
   }
+
+  // Clear keybords input
+  keysPressed = [];
+
   for (let i = 0; i < charWord.length; i++) {
     const box = document.createElement('div');
     box.className = 'letter-box';
@@ -58,19 +69,21 @@ function newGame() {
   letterButtons.forEach(button => {
     button.classList.remove('wrong');
     button.classList.remove('right');
-
-    guessDisplay.textContent = '0 / 6';
+  });
+  guessDisplay.textContent = '0 / 6';
+  svgId.forEach(id => {
+    id.style.display = 'none';
   });
 }
 newGame();
+
 // GAMEOVER FUNCTION CALLING MODAL //
 function gameOver(isItOver) {
-  let ng = isItOver;
+  playing = false;
   letterButtons.forEach(button => {
     button.classList.add('wrong');
   });
-
-  if (ng) {
+  if (isItOver) {
     modal.classList.remove('hidden');
     modalHeader.textContent = 'You Win 🎉';
   } else {
@@ -91,44 +104,41 @@ function generateWord() {
     'computer',
   ];
   selectedWord = words[Math.floor(Math.random() * words.length)];
-  console.log(selectedWord);
   return selectedWord;
 }
 
 function print(letter) {
-  let guess = 0;
-  const rightButton = document.getElementById(letter.toLowerCase());
-  charWord.forEach((char, i) => {
-    if (char === letter.toLowerCase()) {
-      document.getElementById(i + 1).textContent = char;
-      guess = 1;
-      rightButton.classList.add('right');
-      rightGuesses++;
-      console.log(rightGuesses);
-      console.log(selectedWord.length);
+  if (playing) {
+    let guess = 0;
+    const rightButton = document.getElementById(letter.toLowerCase());
+    charWord.forEach((char, i) => {
+      if (char === letter.toLowerCase()) {
+        document.getElementById(i + 1).textContent = char.toUpperCase();
+        guess = 1;
+        rightButton.classList.add('right');
+        rightGuesses++;
+      }
+    });
+    if (guess == 0) {
+      const wrongButton = document.getElementById(letter.toLowerCase());
+      wrongButton.classList.add('wrong');
+      wrongGuessCounter++;
+      addBodyParts();
     }
-  });
-  if (guess == 0) {
-    const wrongButton = document.getElementById(letter.toLowerCase());
-    wrongButton.classList.add('wrong');
-    wrongGuessCounter++;
-    console.log(wrongGuessCounter);
-    addBodyParts();
-  }
-  guessDisplay.textContent = `${wrongGuessCounter} / ${maxGuesses}`;
-
-  if (wrongGuessCounter === maxGuesses) {
-    gameOver(0);
-  }
-  if (rightGuesses === selectedWord.length) {
-    gameOver(1);
+    keysPressed.push(letter);
+    guessDisplay.textContent = `${wrongGuessCounter} / ${maxGuesses}`;
+    if (wrongGuessCounter === maxGuesses) {
+      gameOver(0);
+    }
+    if (rightGuesses === selectedWord.length) {
+      gameOver(1);
+    }
   }
 }
 
 newGameBtn.addEventListener('click', newGame);
 
 /* DISPLAYING HANGMAN */
-
 function addBodyParts() {
   if (wrongGuessCounter <= maxGuesses) {
     let addPart;
@@ -158,7 +168,6 @@ function addBodyParts() {
         addPart.style.display = 'block';
         break;
       default:
-        console.log('fail');
     }
   }
 }
